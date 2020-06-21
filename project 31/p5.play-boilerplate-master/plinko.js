2e73b1762b94738774e87247ff13255e2a8a47af
katsboi <64533685+katsboi@users.noreply.github.com>
2020-06-21 12:50:50 +0530

    Add files via upload

---
+++ project 31/p5.play-boilerplate-master/plinko.js
@@ -0,0 +1,20 @@
+class Plinko {
+    constructor(x,y) {
+            var options = {
+            isStatic: true,
+            'restitution':0.4
+            }
+
+        this.r=10;
+        this.body = Bodies.circle(x,y,this.r,options);
+        World.add(world, this.body);
+        }
+    display(){
+    var pos =this.body.position;
+    ellipseMode(RADIUS);
+    fill("white");
+    ellipse(pos.x, pos.y, this.r,this.r);
+    
+    }
+}
+    
